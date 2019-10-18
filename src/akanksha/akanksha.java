71d7306709954a1354ea3704b670880a14386451
Akanksha Ashu <Akanksha@gmail.com>
2019-10-18 12:01:07 +0530

    other push

---
+++ src/akanksha/akanksha.java
@@ -0,0 +1,14 @@
+package akanksha;
+
+public class akanksha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a ,b,c;
+		a=10;
+		b=20;
+		c=a+b;
+		System.out.println(+c);
+
+	}
+}
